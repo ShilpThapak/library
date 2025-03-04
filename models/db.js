3d78d3427db5377e8d6e8ab6f2dd8186b6dc57d7
ShilpThapak <thapak.shilp2395@gmail.com>
2025-03-04 20:20:22 +0530

    updated env for supabase integration

--- models/db.js
+++ models/db.js
@@ -6,12 +6,12 @@ dotenv.config();
 
 const sequelize = new Sequelize({
   dialect: PostgresDialect,
-  database: process.env.DB_NAME,      
-  user: process.env.DB_USER,          
-  password: process.env.DB_PASSWORD,  
-  host: process.env.DB_HOST,          
-  port: process.env.DB_PORT || 5432,
-  ssl: process.env.DB_SSL === 'true',
+  database: process.env.POSTGRES_DB,      
+  user: process.env.POSTGRES_USER,          
+  password: process.env.POSTGRES_PASSWORD,  
+  host: process.env.POSTGRES_HOST,          
+  port: process.env.POSTGRES_PORT || 5432,
+  ssl: process.env.POSTGRES_SSL === 'true',
 });
 
-export default sequelize
+export default sequelize;
